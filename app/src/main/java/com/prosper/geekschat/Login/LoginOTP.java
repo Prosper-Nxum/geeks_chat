d7518b8eb79efbda746c8541703c4c9e16131ab2
ProsperNxu <Prosperd97@gmail.com>
2023-12-11 14:40:14 +0200

    Login step 2 OTP

---
+++ app/src/main/java/com/prosper/geekschat/Login/LoginOTP.java
@@ -0,0 +1,16 @@
+package com.prosper.geekschat.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prosper.geekschat.R;
+
+public class LoginOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+    }
+}
