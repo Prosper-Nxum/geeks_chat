7c297352cb1f4d39497ac98731581caaad8cf7c6
ProsperNxu <Prosperd97@gmail.com>
2023-12-11 15:21:46 +0200

    Login update

--- app/src/main/java/com/prosper/geekschat/Login/LoginOTP.java
+++ app/src/main/java/com/prosper/geekschat/Login/LoginOTP.java
@@ -3,14 +3,22 @@ package com.prosper.geekschat.Login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.prosper.geekschat.R;
 
 public class LoginOTP extends AppCompatActivity {
 
+    String phoneNumber;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
+
+        //Printing back the number
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/prosper/geekschat/Login/PhoneLogin.java
+++ app/src/main/java/com/prosper/geekschat/Login/PhoneLogin.java
@@ -2,15 +2,48 @@ package com.prosper.geekschat.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.hbb20.CountryCodePicker;
 import com.prosper.geekschat.R;
 
 public class PhoneLogin extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneInput;
+    Button sendOtpBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_login);
+
+        countryCodePicker = findViewById(R.id.login_countrycode);
+        phoneInput = findViewById(R.id.login_mobile_number);
+        sendOtpBtn = findViewById(R.id.send_otp_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+
+        progressBar.setVisibility(View.GONE);
+
+        //Link country code with Text field (phone number)
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+        sendOtpBtn.setOnClickListener((v)->{
+            if(!countryCodePicker.isValidFullNumber()){
+                phoneInput.setError("Phone number not valid");
+                return;
+            }
+            Intent intent = new Intent(PhoneLogin.this,LoginOTP.class);
+            intent.putExtra("phone",countryCodePicker.getFullNumberWithPlus());
+            startActivity(intent);
+        });
     }
+
+
 }
