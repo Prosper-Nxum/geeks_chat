bf6a5c44e959d35862b8411f215cc1370328c582
ProsperNxu <Prosperd97@gmail.com>
2023-12-12 07:02:31 +0200

    manifest

--- app/src/main/java/com/prosper/geekschat/SplashScreen.java
+++ app/src/main/java/com/prosper/geekschat/SplashScreen.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.prosper.geekschat.Login.PhoneLogin;
+import com.prosper.geekschat.login.PhoneLogin;
 
 public class SplashScreen extends AppCompatActivity {
 
